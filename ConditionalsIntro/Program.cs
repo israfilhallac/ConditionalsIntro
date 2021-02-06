ca6582944e47f68c6930690964370754d90a2530
israfilhallac <i.hallac1408@gmail.com>
2021-02-06 14:13:31 +0200

    Add project files.

---
+++ ConditionalsIntro/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConditionalsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 11;
+
+            if (number >= 0 && number <= 100)
+            {
+                Console.WriteLine("Number is between 0-100");
+            }
+            else if (number > 100 && number <= 200)
+            {
+                Console.WriteLine("Number is between 101-200");
+            }
+            else if (number > 200 || number < 0)
+
+            {
+                Console.WriteLine("Number is less than 0 or greater than 200");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
